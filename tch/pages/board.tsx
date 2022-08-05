5aae0d4b04f261f856a24c29aba7dad474411586
izcoser <76838137+izcoser@users.noreply.github.com>
2022-08-04 23:45:24 -0300

    Use Tailwind
    Just playing with tailwind for now. Seems really cool

--- tch/pages/board.tsx
+++ tch/pages/board.tsx
@@ -16,16 +16,11 @@ export default function Board() {
   if (error) return <div>failed to load</div>
   if (!data) return <div>loading...</div>
   return (
-    <div className="postList">
+    <div className="color-pink grid justify-items-center grid-cols-1 gap-5">
       {data.map((p: PostResponse) =>
-
-        <div className="post" key={p.id}>
-          <div className="postTitle">
-            {p.title}
-          </div>
-          <div className="postBody">
-            {p.body}
-          </div>
+        <div className="w-8/12 text-center rounded-md border bg-purple-900" key={p.id}>
+          <div className="text-fuchsia-200 text-2xl"><span className="text-slate-900/100">No. {p.id}</span> {p.title}</div>
+          <div className="text-indigo-200">{p.body}</div>
         </div>)}
     </div>
   )
